ce495bf44e47add35ecf0afb4e2b0a4ffc41c5d3
yamsou <42236695+yamsou@users.noreply.github.com>
2018-09-02 00:50:50 +0200

    test dms

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ const twitterApp = {
   consumer_secret: process.env.CONSUMER_SECRET,
   access_token: process.env.ACCESS_TOKEN,
   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+  webhook_env: process.env.WEBHOOK_ENV,
   timeout_ms: 60 * 1000 // optional HTTP request timeout to apply to all requests.
 };
 
