7c585bb0566bac37a723da1d3054816ac0ddb496
yamsou <42236695+yamsou@users.noreply.github.com>
2018-09-02 03:50:29 +0200

    test dms

--- src/AutoDM.js
+++ src/AutoDM.js
@@ -12,11 +12,11 @@ const AutoDM = () => {
       console.log("envoi " + data);
       console.log(err);
     });
-  const data2 = T.get('direct_messages/events/show', {"id": "1033956357559537668"}, 
-    function (err, data, response) {
-      console.log("get 1 dm "+ data.event.message_create.message_data.text);
-      console.log(err);
-    });
+  //const data2 = T.get('direct_messages/events/show', {"id": "1033956357559537668"}, 
+  //  function (err, data, response) {
+  //    console.log("get 1 dm "+ data.event.message_create.message_data.text);
+  //    console.log(err);
+  //  });
   //const data3 = T.get('direct_messages/events/list', 
   //  function (err, data, response) {
   //    console.log(data.events);
@@ -24,7 +24,7 @@ const AutoDM = () => {
   //});
   const data3 = T.get('users/show', {"screen_name": "yorozuyams"}, 
     function (err, data, response) {
-      console.log("get 1 user "+ data.id);
+      console.log("get 1 user "+ data.id_str);
       console.log(err);
   });
 
